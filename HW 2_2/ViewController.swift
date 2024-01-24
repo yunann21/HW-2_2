e3491dd50e0f54f251337ccf6ba82ec527bc2b63
Anna Ablogina <yunann95@gmail.com>
2024-01-24 13:33:03 +0300

    Initial Commit

---
+++ HW 2_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2_2
+//
+//  Created by Anna Ablogina on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
